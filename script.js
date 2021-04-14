abb2213cb06688a75937b4d99214cc4fe0a93e54
junaid ahamad <erjunaidahamad@gmail.com>
2021-04-14 20:16:18 +0530

    Count down Timer

---
+++ script.js
@@ -0,0 +1,22 @@
+let newDate = "1 Jan 2023";
+
+function countdown() {
+  let newYearDate = new Date(newDate);
+  let currentDate = new Date();
+  let seconds = (newYearDate - currentDate) / 1000;
+
+  let days = Math.floor(seconds / 3600 / 24);
+  let hours = Math.floor(seconds / 3600) % 24;
+  let minutes = Math.floor((seconds / 60) % 60);
+  let seco = Math.floor(seconds % 60);
+
+  document.querySelector("#days").innerHTML = days;
+  document.querySelector("#hours").innerHTML = formatTime(hours);
+  document.querySelector("#minutes").innerHTML = formatTime(minutes);
+  document.querySelector("#seconds").innerHTML = formatTime(seco);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+setInterval(countdown, 1000);
